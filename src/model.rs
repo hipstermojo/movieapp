ac394a5e8abe0378188ac1125ad44e838aa219c0
Kenneth Gitere <gitere81@gmail.com>
2019-07-08 12:32:53 +0300

    Implement User model and database interaction for adding users

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,15 @@
+use mongodb::coll::results::InsertOneResult;
+use mongodb::db::{Database, ThreadedDatabase};
+use mongodb::oid::ObjectId;
+use mongodb::Error;
+use r2d2::{Pool, PooledConnection};
+use r2d2_mongodb::MongodbConnectionManager;
+
+pub type APIKey = String;
+
+pub type MongoPool = Pool<MongodbConnectionManager>;
+type MongoPooledConnection = PooledConnection<MongodbConnectionManager>;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct APIMovieData {
     pub backdrop_path: String,
@@ -15,4 +27,32 @@ pub struct APIResponse {
     pub results: Vec<APIMovieData>,
 }
 
-pub type APIKey = String;
+#[derive(Debug, Serialize)]
+pub struct User {
+    pub _id: ObjectId,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+impl User {
+    pub fn create(new_user: NewUserForm, pool: &MongoPool) -> Result<InsertOneResult, Error> {
+        let db_conn: &Database = &get_db_conn(pool).unwrap();
+        let users_coll = db_conn.collection("users");
+        users_coll.insert_one(
+            doc! {"name":new_user.name,"email":new_user.email,"password":new_user.password},
+            None,
+        )
+    }
+}
+
+fn get_db_conn(pool: &MongoPool) -> Result<MongoPooledConnection, &'static str> {
+    pool.get().map_err(|_| "Can't get database connection")
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NewUserForm {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
